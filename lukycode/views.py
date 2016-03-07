0073d904e894f5f59d807ec43e775d1f9e7a5dcd
abdulmanan <lukyman>
2016-03-07 22:48:20 +0300

    User profile template aka lukyme

--- lukycode/views.py
+++ lukycode/views.py
@@ -6,3 +6,6 @@ def index(request):
 	
 	return render(request,'lukynote.html')
 
+def lukyme(request):
+
+	return render(request,'lukyme.html')
--- lukyman/urls.py
+++ lukyman/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^','lukycode.views.index'),
-    url(r'^profile','lukycode.views.profile')
+    url(r'^$','lukycode.views.index'),
+    url(r'^lukyme','lukycode.views.lukyme')
 ]
